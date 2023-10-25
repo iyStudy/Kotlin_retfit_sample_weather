d3a98a4eddaa03da0df8c930a314e06384f78550
iyStudy <ishidayuta.recruit@gmail.com>
2023-10-25 18:53:23 +0900

    mod ハンズオンの完成版

--- app/src/main/java/com/example/kotlin_retfit_sample_weather/WeatherListFragment.kt
+++ app/src/main/java/com/example/kotlin_retfit_sample_weather/WeatherListFragment.kt
@@ -22,22 +22,10 @@ class WeatherListFragment : Fragment() {
     private lateinit var binding: FragmentWeatherListBinding
 
     // OpenWeatherMapのAPIアクセスに必要なキー
-    private val API_KEY = "自分のkey"
+    private val API_KEY = "自分のAPI Key"
 
     // 対象となる日本の都市のリスト
-    private val cities = listOf(
-        "Tokyo",
-        "Osaka",
-        "Kyoto",
-        "Hiroshima",
-        "Fukuoka",
-        "Hokkaido",
-        "Okinawa",
-        "Aomori",
-        "Nagano",
-        "Tottori",
-        "Nagoya"
-    )
+    private val cities = listOf("Tokyo", "Osaka", "Kyoto", "Hiroshima", "Fukuoka", "Hokkaido", "Okinawa", "Aomori", "Nagano", "Tottori",)
 
     // 取得した天気情報を保存するためのリスト
     private val weatherList = mutableListOf<Weather>()
@@ -76,42 +64,39 @@ class WeatherListFragment : Fragment() {
         val api = retrofit.create(WeatherApi::class.java)  // 定義したAPIインターフェースからインスタンスを作成
 
         // 各都市の天気情報をAPIから取得
-        cities.forEach { city ->
-            api.fetchWeather(city, "ja", API_KEY).enqueue(object : Callback<Weather> {
-                // APIリクエストが成功したときの処理
-                override fun onResponse(call: Call<Weather>, response: Response<Weather>) {
-                    response.body()?.let { weather ->
-                        weatherList.add(weather)  // 取得した天気情報をリストに追加
-                        completedRequests++  // 成功したリクエストの数をカウントアップ
+        api.fetchWeather("Kyoto", "ja", API_KEY).enqueue(object : Callback<Weather> {
+            // APIリクエストが成功したときの処理
+            override fun onResponse(call: Call<Weather>, response: Response<Weather>) {
+                response.body()?.let { weather ->
+                    weatherList.add(weather)  // 取得した天気情報をリストに追加
+                    completedRequests++  // 成功したリクエストの数をカウントアップ
 
-                        // 全ての都市の天気情報が取得できた場合
-                        if (completedRequests == cities.size) {
-                            // RecyclerViewのアダプターを設定して、取得したデータを表示
-                            binding.rvCitylist.adapter =
-                                WeatherAdapter(weatherList) { selectedWeather ->
-                                    // 選択された天気情報を格納
-                                    parentFragmentManager.setFragmentResult(
-                                        REQUEST_WEATHER_DETAIL,
-                                        bundleOf(SELECTED_WEATHER to selectedWeather)
-                                    )
-                                    // 各都市のアイテムをクリックした時の動作を定義
-                                    val fragment = WeatherDetailsFragment()
-                                    parentFragmentManager.beginTransaction()
-                                        .setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left, R.anim.slide_in_left, R.anim.slide_out_right)
-                                        .replace(R.id.fragmentContainer, fragment)  // 新しいフラグメントに切り替える
-                                        .addToBackStack(null)  // バックスタックに追加して、戻るボタンで前のフラグメントに戻れるようにする
-                                        .commit()  // 変更を確定する
-                                }
+                    // 全ての都市の天気情報が取得できた場合
+
+                    // RecyclerViewのアダプターを設定して、取得したデータを表示
+                    binding.rvCitylist.adapter =
+                        WeatherAdapter(weatherList) { selectedWeather ->
+                            // 選択された天気情報を格納
+                            parentFragmentManager.setFragmentResult(
+                                REQUEST_WEATHER_DETAIL,
+                                bundleOf(SELECTED_WEATHER to selectedWeather)
+                            )
+                            // 各都市のアイテムをクリックした時の動作を定義
+                            val fragment = WeatherDetailsFragment()
+                            parentFragmentManager.beginTransaction()
+                                .replace(R.id.fragmentContainer, fragment)  // 新しいフラグメントに切り替える
+                                .addToBackStack(null)  // バックスタックに追加して、戻るボタンで前のフラグメントに戻れるようにする
+                                .commit()  // 変更を確定する
                         }
-                    }
-                }
 
-                // APIリクエストが失敗したときの処理
-                override fun onFailure(call: Call<Weather>, t: Throwable) {
-                    Log.e("API_ERROR", "Failed to fetch weather data", t)  // エラーログを出力
                 }
-            })
-        }
+            }
+
+            // APIリクエストが失敗したときの処理
+            override fun onFailure(call: Call<Weather>, t: Throwable) {
+                Log.e("API_ERROR", "Failed to fetch weather data", t)  // エラーログを出力
+            }
+        })
     }
 
     companion object {
